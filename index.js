190caf7c64f27adbf6a17f719ed722e81f3354c8
eshwagadkar <eshwagadkar007@gmail.com>
2024-02-22 20:15:09 +0530

    clear redundant ws

--- index.js
+++ index.js
@@ -5,9 +5,7 @@ const schema = require('./schema/schema')
 
 const app = express()
 
-app.all('/graphql', createHandler({ 
-    schema,
- }));
+app.all('/graphql', createHandler({ schema }));
 
 app.listen(3000, () => {
     console.log(`The port is up and running`)
