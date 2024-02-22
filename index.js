e7be90cc27db55ee035b808e87f79ff3f0611d1e
eshwagadkar <eshwagadkar007@gmail.com>
2024-02-22 19:30:42 +0530

    register graphql-http middleware with express server

--- index.js
+++ index.js
@@ -1,6 +1,13 @@
 const express = require('express')
+const { createHandler } = require('graphql-http/lib/use/express')
+
+const schema = require('./schema/schema')
 
 const app = express()
+
+app.all('/graphql', createHandler({ 
+    schema,
+ }));
 
 app.listen(3000, () => {
     console.log(`The port is up and running`)
