c555d8ca6f5418e630400bc2550d012fade96593
eshwagadkar <eshwagadkar007@gmail.com>
2024-02-24 20:13:07 +0530

    initial index file commit

--- index.js
+++ index.js
@@ -1,16 +1,17 @@
 const express = require('express')
+const mongoose = require('mongoose')
+const createHandler = require('graphql-http/lib/use/express')
+
+
 const connectDB = require('./mongoConnect')
 
 require('dotenv').config()
-
 const PORT = process.env.PORT
+
 
 connectDB()
 
 const app = express()
-
-
-
 
 app.listen(PORT, () => {
     console.log(`The express server is up and running on port: ${PORT}`)
